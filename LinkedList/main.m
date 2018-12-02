673f005d6a98132aeb774cda610418e62dd43a77
Dima <dima@Dimas-Mac.local>
2018-12-02 14:45:20 +0300

    Initial Commit

---
+++ LinkedList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LinkedList
+//
+//  Created by Dima  on 12/2/18.
+//  Copyright © 2018 Dima . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
